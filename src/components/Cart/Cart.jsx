673a7ee8f38357563337b75bf9c0fc047bb69e92
MyuB <mjycoco0@naver.com>
2022-12-25 16:58:47 +0900

    cart modal and reducer added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[
@@ -11,23 +12,25 @@ const Cart = () => {
           price: 12.99,
         },
       ].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}></button>
-        <button className={classes.button}></button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          close
+        </button>
+        <button className={classes.button}>order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 

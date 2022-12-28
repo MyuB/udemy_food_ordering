7291e58798b7d93f6a27a3715c8f45add731773f
MyuB <mjycoco0@naver.com>
2022-12-28 20:22:56 +0900

    http error state added

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -14,6 +14,11 @@ const AvailableMeals = () => {
       const response = await fetch(
         "https://react-http-8e004-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -31,13 +36,26 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals()
+      .then()
+      .catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
   }, []);
 
   if (isLoading) {
     return (
       <section>
         <p className={classes.MealsIsLoading}>Loading Datas.....</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.MealsError}>{httpError}</p>
       </section>
     );
   }
